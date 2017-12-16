d6c248d605b5733d03cb0bcf0bdc8c3d58c05464
Chase <chaseit12@gmail.com>
2017-12-16 22:44:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function theBeatlesPlay(musicians,instruments){
+   var arr = []
+
+   for(let i = 0; i < musicians.length;i++){
+       arr.push(musicians[i] + ' plays ' + instruments[i])
+   }
+   return arr
+}
